1118cad08d25925654ebe657449c13e9732053e3
Utkarsh Sharma <utkarshofficialin@gmail.com>
2023-07-22 00:26:40 +0530

    add task working and saving task to localstorage

--- modules/todo/todo-style.js
+++ modules/todo/todo-style.js
@@ -1,24 +1,12 @@
 // for removing wish
-setTimeout(()=>{
-    wish.style.display="none";
-},2000);
+setTimeout(() => {
+  wish.style.display = "none";
+}, 2000);
 
-// when clicked on addTask
-addTaskBtn.addEventListener('click',()=>{
-    let img = addTaskBtn.children[0];
-    if(img.src.lastIndexOf("add.svg")!=-1){
-        img.src = "img/tick.svg";
-        img.style.width = "45px";
-        addTaskForm.style.display = "block";
-    }else{
-        img.style.width = "25px";
-        img.src = "img/add.svg";
-        addTaskForm.style.display = "none";
-    }
-});
 
-cancelTask.onclick = ()=>{
-    addTaskBtn.children[0].style.width = "25px";
-    addTaskBtn.children[0].src = "img/add.svg";
-    addTaskForm.style.display = "none";
-}
+cancelTask.onclick = () => {
+  addTaskBtn.children[0].style.width = "25px";
+  addTaskBtn.children[0].src = "img/add.svg";
+  addTaskForm.style.display = "none";
+};
+
--- modules/todo/todo.js
+++ modules/todo/todo.js
@@ -1,30 +1,30 @@
-let user = JSON.parse(localStorage.getItem("user"));
 
+let user = JSON.parse(localStorage.getItem("user"));
 let taskList = user.taskList;
 
 class Task {
-  constructor(title, description, isActive, date, fullname) {
+  constructor(title, description, isActive, date=null) {
     this.title = title;
     this.description = description;
     this.isActive = isActive;
-    this.avatarLetter = fullname[0];
+    this.avatarLetter = user.fullname[0];
     this.date = date;
   }
   toElement = () => {
     return `<li>
         <div class="taskCard">
           <div class="avatarOnCard">
-            <div id="taskAvatar" class="avatar" >
+            <div class="avatar" >
               ${this.avatarLetter}
             </div>
             <div class="taskCardDone">
-              <button id="taskCardBtn">
+              <button>
                 <img class="tick" src="img/tick.svg" alt="" />
               </button>
             </div>
           </div>
           <div class="taskTitle">
-            <p id="task">${this.title}</p>
+            <p>${this.title}</p>
           </div>
           <hr />
           <div class="taskDescription">
@@ -39,7 +39,7 @@ class Task {
 }
 
 class Tasks {
-  constructor(taskList){
+  constructor(taskList) {
     this.taskList = taskList;
     this.update();
   }
@@ -47,12 +47,16 @@ class Tasks {
   done = [];
   activeStr = "";
   doneStr = "";
+  fullname = user.fullname;
   addTask = (task) => {
     taskList.unshift(task);
     this.update();
   };
-  removeTask=()=>{
+  saveTaskList=()=>{
+    user.taskList = this.taskList;
+    localStorage.setItem("user",JSON.stringify(user));
   }
+  removeTask = () => {};
   update = () => {
     this.active = [];
     this.done = [];
@@ -66,7 +70,36 @@ class Tasks {
         this.doneString += taskList[i].toElement();
       }
     }
+    this.saveTaskList();
   };
 }
 
+let tasks = new Tasks(taskList);
+
+// when clicked on addTask toggle the btn
+addTaskBtn.addEventListener("click", () => {
+  let img = addTaskBtn.children[0];
+  if (img.src.lastIndexOf("add.svg") != -1) {
+    img.src = "img/tick.svg";
+    img.style.width = "45px";
+    addTaskForm.style.display = "block";
+  } else {
+    if (addTitle.value.length == 0 || addDescription.value.length == 0) return;
+    
+    if (addTaskBtn.children[0].src.lastIndexOf("tick.svg") != -1) {
+      if(addTitle.value.length == 0 || addDescription.value.length ==0)
+        return;
+  
+      // now add this task as new active task
+      tasks.addTask(new Task(addTitle.value,addDescription.value,true));
+      console.log("work");
+      // update all data
+      tasks.update();
+    }
+
+    img.style.width = "25px";
+    img.src = "img/add.svg";
+    addTaskForm.style.display = "none";
+  }
+});
 
