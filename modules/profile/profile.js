c65c3281fd5033ebf859d57a374fd0a9a5b6bcf0
Utkarsh Sharma <utkarshofficialin@gmail.com>
2023-07-23 02:06:00 +0530

    fixed profile editing for duplicate values

--- modules/profile/profile.js
+++ modules/profile/profile.js
@@ -14,14 +14,40 @@ function readUserDetailsToDOM(){
 }
 
 function saveUserDetails(){
+    let borderColor = "rgba(55, 65, 81, 1)";
+    let i;
     showLoader();
     hideLoader(800);
     user.fullname = fullname.value;
-    user.username = username.value;
-    user.email = email.value;
+    
+    // check if username already exists or not
+    for(i=0;i<users.length;i++){
+        // && also check that it is same user or not if same user than ignore
+        if(users[i].username == username.value && users[i].username != user.username){
+            username.style.borderColor = "red";
+            break;
+        }
+    }
+    if(i==users.length){
+        username.style.borderColor = borderColor;
+        user.username = username.value;
+    }
+    // check if email already exists or not
+    for(i=0;i<users.length;i++){
+        if(users[i].email == email.value && users[i].email != user.email){
+            email.style.borderColor = "red";
+            console.log(users[i]);
+            break;
+        }
+    }
+    if(i==users.length){
+        email.style.borderColor = borderColor;
+        user.email = email.value;
+    }
+    
     user.password = password.value;
     localStorage.setItem("user",JSON.stringify(user));
-    for(let i=0;i<users.length;i++){
+    for(i=0;i<users.length;i++){
         if(users[i].username == user.username){
             users[i] = user;
             break;
@@ -68,4 +94,6 @@ deleteTaskBtn.addEventListener("click",deleteAllTask);
 
 window.onload = ()=>{
     readUserDetailsToDOM();
+    // profile image
+    profTitle.innerHTML = user.fullname[0];
 }
