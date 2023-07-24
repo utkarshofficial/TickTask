2b5a15b8cb9a1a86da4aefe96628fe523729c5eb
Utkarsh Sharma <utkarshofficialin@gmail.com>
2023-07-24 14:24:32 +0530

    home page added

--- modules/todo/todo.js
+++ modules/todo/todo.js
@@ -3,7 +3,7 @@ let user = JSON.parse(localStorage.getItem("user"));
 
 // ! user not logged in -> redirect to login
 if(user == null){
-  location.href = location.origin + "/modules/login/login.html";
+  location.href = location.origin + "./modules/login/login.html";
 }
 
 let users = JSON.parse(localStorage.getItem("users"));
@@ -25,9 +25,17 @@ class Task {
     this.avatarLetter = user.fullname[0];
     // this.date = date;
   }
+  taskCardColorGenerator(){
+    let color = '';
+    let hexColor = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'];
+    for(let i=0;i<6;i++){
+      color += hexColor[Math.floor(Math.random() * hexColor.length)];
+    }
+    return color;
+  }
   toElement(){
     return `<li>
-        <div class="taskCard">
+        <div class="taskCard" style="background-color: #${this.taskCardColorGenerator()};">
           <div class="avatarOnCard">
             <div class="avatar" >
               ${this.avatarLetter}
